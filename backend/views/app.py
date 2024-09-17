12bd6c6f80f5bbb3daa97cd0343f0284e63c1c12
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-09-17 17:10:44 +0300

    added the yaml files which contain the documentation for the various endpoints

--- backend/views/app.py
+++ backend/views/app.py
@@ -1,3 +1,5 @@
+#!usr/bin/python3
+"""Module to start the flask app"""
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -7,19 +9,27 @@ from auth import auth
 from views.Spotify import spotify
 from flask_swagger_ui import get_swaggerui_blueprint
 
-SWAGGER_URL = '/docs/'
-API_URL = '/swagger.yaml'
-swagger_ui_blueprint = get_swaggerui_blueprint(SWAGGER_URL, API_URL)
+AUTH_URL = '/docs/auth'
+AUTH_API_URL = os.path.join(os.path.dirname(__file__), '/static/docs/auth_swagger.yaml')
+auth_swagger_ui_blueprint = get_swaggerui_blueprint(AUTH_URL, AUTH_API_URL,config={'app_name': "AUTH API"})
+
+LLM_URL = '/docs/llm'
+LLM_API_URL = os.path.join(os.path.dirname(__file__), '/static/docs/llm-swagger.yaml')
+llm_swagger_ui_blueprint = get_swaggerui_blueprint(LLM_URL, LLM_API_URL,config={'app_name': "LLM API"})
+
+SPOTIFY_URL = '/docs/spotify'
+SPOTIFY_API_URL = os.path.join(os.path.dirname(__file__), '/static/docs/spotify-swagger.yaml')
+spotify_swagger_ui_blueprint = get_swaggerui_blueprint(SPOTIFY_URL, SPOTIFY_API_URL,config={'app_name': "Spotify API"})
 
 app = Flask(__name__)
-app.secret_key = os.urandom(24)
 
 # Register blueprints with the app
-#app.register_blueprint(auth)
 app.register_blueprint(groq)
 app.register_blueprint(auth)
 app.register_blueprint(spotify)
-app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
+app.register_blueprint(auth_swagger_ui_blueprint, url_prefix=AUTH_URL, name='auth_swagger_ui')
+app.register_blueprint(llm_swagger_ui_blueprint, url_prefix=LLM_URL, name='llm_swagger_ui')
+app.register_blueprint(spotify_swagger_ui_blueprint, url_prefix=SPOTIFY_URL, name='spotify_swagger_ui')
 
 
 if __name__ == '__main__':
